1cf82b315394df088ff558ea228c37481d9c0291
vallemdanaiah <142139815+vallemdanaiah@users.noreply.github.com>
2025-04-02 21:41:35 +0530

    Update settings.py

--- Navyait/settings.py
+++ Navyait/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -122,7 +122,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "assets/static",
     
 ]
-STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
